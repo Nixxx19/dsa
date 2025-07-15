c3054ea658d20f08c64c16a274507b76a5e05693
nityam <nixxspeaks@gmail.com>
2025-07-15 22:40:29 +0530

    Time: 0 ms (100.00%) | Memory: 16.8 MB (61.27%) - LeetSync

---
+++ 2058-concatenation-of-array/concatenation-of-array.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        vector<int> final_arr = nums;
+        for(int i =0;i<nums.size();i++){
+            final_arr.push_back(nums[i]);
+        }
+        return final_arr;
+    }
+};
