1157e363324a180fbf4b823df201b0a2a6104d02
nityam <nixxspeaks@gmail.com>
2025-08-02 23:39:23 +0530

    Time: 3 ms (65.64%) | Memory: 26.8 MB (68.77%) - LeetSync

--- 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.cpp
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
-    vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) 
-    {
-        int n = grid.size();
+    vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
+       int n = grid.size();
         int total = n * n;
-        vector<int> freq(total + 1, 0);
+        vector<int> freq(total + 1, 0);  
+        int repeated = -1, missing = -1;
 
-        for (const auto& row : grid) // Counting frequency of each number
-        {
-            for (int num : row) {
-                freq[num]++;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                freq[grid[i][j]]++;
             }
         }
-        int repeated = 0, missing = 0;
-        for (int i = 1; i <= total; ++i) 
-        {
-            if (freq[i] == 2) repeated = i;
+
+        for (int i = 1; i <= total; i++) {
             if (freq[i] == 0) missing = i;
+            else if (freq[i] == 2) repeated = i;
         }
-        return {repeated,missing}; // Whatever satisfies the condition
+
+        return {repeated, missing};
     }
 };
