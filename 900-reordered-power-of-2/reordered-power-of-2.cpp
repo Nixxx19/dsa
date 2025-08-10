8ef2eb792d5d172783346f198e7e409e3d63ed4f
nityam <nixxspeaks@gmail.com>
2025-08-10 23:56:24 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (34.22%) - LeetSync

--- 900-reordered-power-of-2/reordered-power-of-2.cpp
+++ 900-reordered-power-of-2/reordered-power-of-2.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
-    string getSortedDigits(int num) 
-    {
+    // Returns the sorted digit signature of a number
+    string getSortedDigits(int num) {
         string digits = to_string(num);
         sort(digits.begin(), digits.end());
         return digits;
     }
 
-    bool reorderedPowerOf2(int n) 
-    {
+    bool reorderedPowerOf2(int n) {
         string targetDigits = getSortedDigits(n);
 
-        for (int i = 0; i < 30; ++i) 
-        {
+        // Loop through powers of 2 from 2^0 to 2^29 (since 2^30 > 1e9)
+        for (int i = 0; i < 30; ++i) {
             int powerOfTwo = 1 << i;
-            string powerDigits = getSortedDigits(powerOfTwo);
-
-            if (powerDigits == targetDigits)
+            if (getSortedDigits(powerOfTwo) == targetDigits)
                 return true;
         }
 
