b917a6c3d77786433faad0be797b95f32a74acbd
nityam <nixxspeaks@gmail.com>
2025-08-11 00:08:00 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (34.22%) - LeetSync

--- 900-reordered-power-of-2/reordered-power-of-2.cpp
+++ 900-reordered-power-of-2/reordered-power-of-2.cpp
@@ -1,45 +1,21 @@
 class Solution {
 public:
-    bool isPowerOfTwo(int number) 
-    {
-        return number > 0 && (number & (number - 1)) == 0;
-    }
+    bool reorderedPowerOf2(int n) {
+        string s = sortDigits(n);
 
-    bool tryAllPermutations(string& inputDigits, vector<bool>& isUsed, string& currentNumber) 
-    {
-        if (currentNumber.size() == inputDigits.size()) 
-        {
-            if (currentNumber[0] == '0') return false; 
-            int num = stoi(currentNumber);
-            return isPowerOfTwo(num);
+        for(int i = 0; i<30; i++){
+            int power = 1 << i;
+            if(s == sortDigits(power)){
+                return true;
+            }
         }
-
-        for (int i = 0; i < inputDigits.size(); ++i)
-        {
-            if (isUsed[i]) continue;
-
-            if (i > 0 && inputDigits[i] == inputDigits[i - 1] && !isUsed[i - 1]) continue;
-
-            isUsed[i] = true;
-            currentNumber.push_back(inputDigits[i]);
-
-            if (tryAllPermutations(inputDigits, isUsed, currentNumber)) return true;
-
-            currentNumber.pop_back();
-            isUsed[i] = false;
-        }
-
         return false;
     }
 
-    bool reorderedPowerOf2(int n) 
-    {
-        string digits = to_string(n);
-        sort(digits.begin(), digits.end()); 
 
-        vector<bool> used(digits.size(), false);
-        string builtNumber = "";
-
-        return tryAllPermutations(digits, used, builtNumber);
+           string sortDigits(int num) {
+        string s = to_string(num);
+        sort(s.begin(), s.end());
+        return s;
     }
 };
