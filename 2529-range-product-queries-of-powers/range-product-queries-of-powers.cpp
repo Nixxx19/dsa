f6a4e5078f96ef931bb60dec0e561264a05bbc54
nityam <nixxspeaks@gmail.com>
2025-08-11 23:34:11 +0530

    Time: 16 ms (73.40%) | Memory: 147.7 MB (50.53%) - LeetSync

--- 2529-range-product-queries-of-powers/range-product-queries-of-powers.cpp
+++ 2529-range-product-queries-of-powers/range-product-queries-of-powers.cpp
@@ -2,25 +2,47 @@ class Solution {
 public:
     const int MOD = 1e9 + 7;
 
-    vector<int> productQueries(int n, vector<vector<int>>& queries) {
-        vector<int> powers;
-        
-        // Get powers of 2 from binary representation
-        for (int i = 0; i < 31; ++i) {
-            if ((n >> i) & 1) {
-                powers.push_back(1 << i);
+    vector<int> productQueries(int n, vector<vector<int>>& queries) 
+    {
+        vector<int> exponents;
+
+        for (int i = 0; i < 31; ++i) 
+        {
+            if ((n >> i) & 1) 
+            {
+                exponents.push_back(i);
             }
         }
 
-        vector<int> ans;
-        for (auto& q : queries) {
-            int l = q[0], r = q[1];
-            long long prod = 1;
-            for (int i = l; i <= r; ++i) {
-                prod = (prod * powers[i]) % MOD;
-            }
-            ans.push_back((int)prod);
+        int sz = exponents.size();
+        vector<int> prefixSum(sz);
+        prefixSum[0] = exponents[0];
+        for (int i = 1; i < sz; ++i) 
+        {
+            prefixSum[i] = prefixSum[i - 1] + exponents[i];
         }
+
+        vector<int> ans;
+        for (auto& q : queries) 
+        {
+            int l = q[0], r = q[1];
+            int sumExp = prefixSum[r] - (l > 0 ? prefixSum[l - 1] : 0);
+            ans.push_back(modPow(2, sumExp, MOD));
+        }
+
         return ans;
+    }
+
+    int modPow(int base, int exp, int mod) 
+    {
+        long long result = 1;
+        long long b = base;
+        while (exp > 0) 
+        {
+            if (exp % 2 == 1) result = (result * b) % mod;
+            b = (b * b) % mod;
+            exp /= 2;
+        }
+        return result;
     }
 };
