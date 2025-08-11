9fb4f2e59fd409fdfc802d25b1fa7dd8e1e9f259
nityam <nixxspeaks@gmail.com>
2025-08-11 23:36:57 +0530

    Time: 19 ms (65.43%) | Memory: 147.6 MB (58.51%) - LeetSync

--- 2529-range-product-queries-of-powers/range-product-queries-of-powers.cpp
+++ 2529-range-product-queries-of-powers/range-product-queries-of-powers.cpp
@@ -2,47 +2,31 @@ class Solution {
 public:
     const int MOD = 1e9 + 7;
 
-    vector<int> productQueries(int n, vector<vector<int>>& queries) 
+    vector<int> productQueries(int num, vector<vector<int>>& queryList) 
     {
-        vector<int> exponents;
-
-        for (int i = 0; i < 31; ++i) 
+        vector<int> powerList;
+        
+        
+        for (int i = 0; i <= 30; ++i) 
         {
-            if ((n >> i) & 1) 
+            if ((num >> i) & 1) 
             {
-                exponents.push_back(i);
+                powerList.push_back(1 << i);
             }
         }
 
-        int sz = exponents.size();
-        vector<int> prefixSum(sz);
-        prefixSum[0] = exponents[0];
-        for (int i = 1; i < sz; ++i) 
+        vector<int> result;
+        for (auto& query : queryList) 
         {
-            prefixSum[i] = prefixSum[i - 1] + exponents[i];
+            int left = query[0], right = query[1];
+            long long product = 1;
+            for (int i = left; i <= right; ++i) 
+            {
+                product = (product * powerList[i]) % MOD;
+            }
+            result.push_back((int)product);
         }
 
-        vector<int> ans;
-        for (auto& q : queries) 
-        {
-            int l = q[0], r = q[1];
-            int sumExp = prefixSum[r] - (l > 0 ? prefixSum[l - 1] : 0);
-            ans.push_back(modPow(2, sumExp, MOD));
-        }
-
-        return ans;
-    }
-
-    int modPow(int base, int exp, int mod) 
-    {
-        long long result = 1;
-        long long b = base;
-        while (exp > 0) 
-        {
-            if (exp % 2 == 1) result = (result * b) % mod;
-            b = (b * b) % mod;
-            exp /= 2;
-        }
         return result;
     }
 };
