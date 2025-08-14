56ccb5ba28ad163414b485e61b79b4eda1f575f6
nityam <nixxspeaks@gmail.com>
2025-08-14 22:24:40 +0530

    Time: 10 ms (97.36%) | Memory: 11.7 MB (83.42%) - LeetSync

--- 2882-ways-to-express-an-integer-as-sum-of-powers/ways-to-express-an-integer-as-sum-of-powers.cpp
+++ 2882-ways-to-express-an-integer-as-sum-of-powers/ways-to-express-an-integer-as-sum-of-powers.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
-    int numberOfWays(int target, int power) 
+    int numberOfWays(int target, int x) 
     {
         const int MOD = 1e9 + 7;
 
         vector<int> dp(target + 1, 0);
         dp[0] = 1;
 
-        for (int base = 1; ; base++) {
-            long long currPower = 1;
-            for (int i = 0; i < power; i++) 
+        for (int base = 1; ; base++) 
+        {
+            long long power = 1; 
+            for (int i = 0; i < x; i++) 
             {
-                currPower *= base;
-                if (currPower > target) break;
+                power *= base;
+                if (power > target) break;
             }
 
-            if (currPower > target) break;
+            if (power > target) break;
 
-            for (int sum = target; sum >= currPower; sum--) 
+            for (int sum = target; sum >= power; sum--)
             {
-                dp[sum] = (dp[sum] + dp[sum - currPower]) % MOD;
+                dp[sum] = (dp[sum] + dp[sum - power]) % MOD;
             }
         }
 
