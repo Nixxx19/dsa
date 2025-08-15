44d1661c2df9c5c137d3d1d162a3847dddd769f9
nityam <nixxspeaks@gmail.com>
2025-08-15 23:18:49 +0530

    Time: 7 ms (18.31%) | Memory: 11.1 MB (16.31%) - LeetSync

--- 38-count-and-say/count-and-say.cpp
+++ 38-count-and-say/count-and-say.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
-    string generate(string current, int step, int target) {
+    string generate(string current, int step, int target)
+    {
         if (step == target) return current;
         string result = "";
         int i = 0;
         while (i < current.length()) {
             int count = 1;
-            while (i + 1 < current.length() && current[i] == current[i + 1]) {
+            while (i + 1 < current.length() && current[i] == current[i + 1]) 
+            {
                 count++;
                 i++;
             }
@@ -16,7 +18,8 @@ public:
         return generate(result, step + 1, target);
     }
 
-    string countAndSay(int n) {
+    string countAndSay(int n) 
+    {
         return generate("1", 1, n);
     }
 };
